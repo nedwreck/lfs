be00491b6b9be5093f5d0a81807b4def31648e00
nedwreck <76977591+nedwreck@users.noreply.github.com>
2024-06-12 17:37:28 +0000

    Create debianhostinit.sh
    Configuration script to get a Debian Live host ready for Ansible

---
+++ debianhostinit.sh
@@ -0,0 +1,18 @@
+!#/bin/bash
+# Debian LFS Host Initialization Script
+
+# Update and upgrade debian host
+sudo apt update -y && \
+sudo apt upgrade -y && \
+sudo apt autoremove -y &&\
+
+#Install ssh for Ansible
+sudo apt install wget ssh -y &&\
+
+# Permit root to log in from SSH and restart SSH
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config &&\
+sudo systemctl restart ssh &&\
+
+# Create a password for the root user
+# Change this password if desired but it will only be set for the live system for the build
+(echo 'password'; echo 'password') | passwd root
